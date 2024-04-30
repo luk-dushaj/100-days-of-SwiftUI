3465af468330f15fecdc4321af7900e3b8608357
luk-dushaj <extents.lie_0y@icloud.com>
2024-04-29 22:00:48 -0400

    Day4
    All information is in Contents.swift in the playground along with the solution

---
+++ Day4.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Day 4 was mostly review from the previous day.
+// But the owner of 100 days of SwiftUI left me a challenge for this day.
+// Here is the challenge and the solution will be my code below it.
+
+/*
+ Now that you’ve met arrays, dictionaries, and sets, I want to pause for a moment to give you chance to solve a small coding challenge. It’s not designed to trip you up, but instead to encourage you to stop for a while and think about what you’ve learned.
+
+ This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
+
+ I’ll provide some hints below, but please take the time to think about a solution before you read them. Trust me: forgetting what you’ve learned then re-learning it actually makes it sink in deeper!
+ */
+
+let array = ["some", "random", "values", "values", "some", "random", "values"]
+let uniqueItems = Set(array)
+print("Number of items in the array: \(array.count)")
+print("Unique items in the array: \(uniqueItems)")
+
+// I actually have to pay for the answer.
+// But I am confident I got this right without any hints.
