9ebc34491bd7f779d369a8e058893b1a3c32cc13
luk-dushaj <extents.lie_0y@icloud.com>
2024-05-12 12:06:40 -0400

    Day17

---
+++ Starting with SwiftUI/Day17.md
@@ -0,0 +1,12 @@
+# Today I learned:
+
+- Paul (instructor) helped me make the WeSplit app *basically a app that you enter your total amount and tip percentage and it calculates then outputs the new total*
+- @FocusState
+- A lot more about modifiers especially the .toolbar one reminds me a bit of javascript with event handlers mainly in combination with the .focused one
+- Overall this was more of a refinement to what yesterday is too because I am starting to at least know the possibilities of SwiftUI like:
+```swift
+Text(totalPerPerson, format: .currency (code:
+                        Locale.current.currency?.identifier ?? "USD" ) )
+```
+I had no idea Text can take in this much arguments.
+I know SwiftUI is composed of a lot of structs and methods but those structs are packed with a lot of features.
--- Starting with SwiftUI/WeSplit/WeSplit/ContentView.swift
+++ Starting with SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -5,17 +5,73 @@
 //  Created by user on 5/11/24.
 //
 
+/*
+ Directions:
+ 
+ In this project we’re going to be building a check-splitting app that you might use after eating at a restaurant – you enter the cost of your food, select how much of a tip you want to leave, and how many people you’re with, and it will tell you how much each person needs to pay.
+ */
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField( "Amount", value: $checkAmount, format: .currency (code:
+                        Locale.current.currency?.identifier ?? "USD") )
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    // To open a new view full of the list
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section ("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency (code:
+                        Locale.current.currency?.identifier ?? "USD" ) )
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
