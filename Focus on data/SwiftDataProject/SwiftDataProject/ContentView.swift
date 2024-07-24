e980ea661667db30921d484b0ab644b0ad1640fc
luk-dushaj <extents.lie_0y@icloud.com>
2024-07-24 13:57:23 -0400

    Day58

--- Focus on data/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ Focus on data/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -6,16 +6,74 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+
+    @State private var showingUpcomingOnly = false
+
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate),
+    ]
+
+    @Query(filter: #Predicate<User> { user in
+        user.name.localizedStandardContains("R") &&
+        user.city == "London"
+    }, sort: \User.name) var users: [User]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
+                .navigationTitle("Users")
+                .toolbar {
+                    Button("Add Samples 1", systemImage: "plus", action: addSamples1)
+
+                    Button("Add Samples 1", systemImage: "plus", action: addSamples2)
+
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by Name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate),
+                                ])
+                            
+                            Text("Sort by Join Date")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name)
+                                ])
+                        }
+                    }
+                }
         }
-        .padding()
+    }
+
+    func addSamples1() {
+        try? modelContext.delete(model: User.self)
+
+        let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+        let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+        let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+        let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+
+        modelContext.insert(first)
+        modelContext.insert(second)
+        modelContext.insert(third)
+        modelContext.insert(fourth)
+    }
+
+    func addSamples2() {
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+
+        modelContext.insert(user1)
+
+        user1.jobs?.append(job1)
+        user1.jobs?.append(job2)
     }
 }
 
---
+++ Focus on data/SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -0,0 +1,42 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by user on 7/24/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct UsersView: View {
+    @Query var users: [User]
+
+    var body: some View {
+        List(users) { user in
+            HStack {
+                Text(user.name)
+
+                Spacer()
+
+                Text(String(user.unwrappedJobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
+        }
+    }
+
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: sortOrder)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
+}
